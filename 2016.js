2d0d04d5f4a268823501b44156c51f874732217e
zm16290 <zm16290@josai.ac.jp>
2018-10-07 21:50:20 -0400

    do

--- 2016.js
+++ 2016.js
@@ -2,5 +2,5 @@ function button(){
   document.getElementById('kawaii').innerHTML='<img src="./img/kudamono.png">'
 }
 function button(){
-  setTimeout(document.getElementById('kawaii').innnerHTML='<img src="./img/kawaii.png">',3000);
+  setTimeout(document.getElementById('kawaii').innnerHTML='<img src="./img/kawaii.png">;',3000);
 }
