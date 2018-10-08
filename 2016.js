d21d62c7146b962a2398fee0938aad72bfc860d7
zm16290 <zm16290@josai.ac.jp>
2018-10-07 21:41:15 -0400

    charenge

--- 2016.js
+++ 2016.js
@@ -1,3 +1,6 @@
 function button(){
   document.getElementById('kawaii').innerHTML='<img src="./img/kudamono.png">'
 }
+function button(){
+  setTimeout(document.getElementById('kawaii').innnerHTML='<img src="./img/kawaii.png">',3000);
+}
