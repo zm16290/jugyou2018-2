658a3b60ffe7a68d3043d29eee5177ccd5a6e39e
zm16290 <zm16290@josai.ac.jp>
2018-10-14 21:46:55 -0400

    no.3

---
+++ ex1997.js
@@ -0,0 +1,7 @@
+function face1(){
+  document.getElementById('face').innerHTML='<img src="./img/ochanoma_mu.png">'
+}
+
+function face2(){
+  document.getElementById('face').innerHTML='<img src="./img/ochanoma_smile.png">'
+}
