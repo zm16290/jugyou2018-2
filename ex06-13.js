e8a55c12995530cd2d234416152116adc91f1a34
zm16290 <zm16290@josai.ac.jp>
2018-11-11 20:44:41 -0500

    no7-2

--- ex06-13.js
+++ ex06-13.js
@@ -1,20 +1,16 @@
-function hekou2(){
-  kaeru2();
-  timer();
-}
-//画像の変更とタイマーのJSをここに指定する
+function henkou2(){
+  timer();  
+  kaeru2(); 
+  }
+
 
 function timer(){
- setInterval("modosu()",3000)
+  setTimeout("modosu()",3000);
 }
-//タイマーを指定
+function modosu() {
+  document.getElementById('man2').src = './img/coldsleep_man.png';
+}
 
-function modosu(){
- document.getElementById('man2').src = './img/coldsleep_man.png';
+function kaeru2() {
+  document.getElementById('man2').src = './img/coldsleep_wakeup_man.png';
 }
-//タイマー後の画像を指定
-
-function kaeru2(){
-  document.getElementById('man2').src = './img/coldsleep_wakeup_man.png' ;
-}
-//画像の変更の指定
