99e3cce74066cbe9d45aa34f6d170d4e9824bc47
zm16290 <zm16290@josai.ac.jp>
2018-11-18 20:55:40 -0500

    No8-2

--- ex07-4.js
+++ ex07-4.js
@@ -2,4 +2,13 @@ function add2(){
   var a = document.getElementById('1input').value;
   var b = document.getElementById('2input').value;
   alert(a/(b*b));
+  
+  //低体重、普通、肥満を表示
+ // if (document.getElementById('1input').value >25 )
+  //{alert('肥満')}
+ // else if(document.getElementById('1input').value > 18.5 )
+//    {alert('普通')}
+//  else{
+//    alert('低体重')
+//  }
 }
