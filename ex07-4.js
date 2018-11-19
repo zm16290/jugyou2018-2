0722ce753887bc25ffdd94634e0fdc699f45653f
zm16290 <zm16290@josai.ac.jp>
2018-11-18 20:45:42 -0500

    No8

---
+++ ex07-4.js
@@ -0,0 +1,5 @@
+function add2(){
+  var a = document.getElementById('1input').value;
+  var b = document.getElementById('2input').value;
+  alert(a/(b*b));
+}
---
+++ ex07-8.js
@@ -0,0 +1,15 @@
+function nazo4(){
+  if(document.getElementById('answer4').value == '夢')
+  {
+    alert('あたり！！');
+  }else if(document.getElementById('answer4').value == 'ゆめ')
+  {
+    alert('あたり！！');
+  } else if(document.getElementById('answer4').value == 'yume')
+    alert('あたり！！');
+  
+  //絶対違う答えの場合
+  else {
+    alert('はずれ！！');
+  }
+}
