76d5a4bb752d22051c52169ca672ad29a9015881
zm16290 <zm16290@josai.ac.jp>
2018-11-25 20:47:54 -0500

    No9

---
+++ ex07-12.js
@@ -0,0 +1,6 @@
+function nazo5(){
+  if(document.getElementById('answer5').value == '夢' || 
+     document.getElementById('answer5').value == 'ゆめ' )
+    {alert('あたり！！')}
+  else{alert('はずれ！！')}
+}
--- ex07-4.js
+++ ex07-4.js
@@ -3,12 +3,10 @@ function add2(){
   var b = document.getElementById('2input').value;
   alert(a/(b*b));
   
-  //低体重、普通、肥満を表示
- // if (document.getElementById('1input').value >25 )
-  //{alert('肥満')}
- // else if(document.getElementById('1input').value > 18.5 )
-//    {alert('普通')}
-//  else{
-//    alert('低体重')
-//  }
+  if(a/(b*b) < 18.5)
+  {alert('低体重')}
+  else if(a/(b*b) < 25 )
+    {alert('普通')}
+  else {alert('肥満')}
+  
 }
