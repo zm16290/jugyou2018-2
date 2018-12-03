6607e91728ae02aaf6898b6571eee845eecdfb81
zm16290 <zm16290@josai.ac.jp>
2018-12-02 20:59:02 -0500

    2018.12.3

---
+++ ex07-13.js
@@ -0,0 +1,12 @@
+function dotira(){
+  if(document.getElementById('toshi').value % 4 == 0 
+    //||
+    //document.getElementById('tohsi').value % 4 == 0
+   // &&
+   // document.getElementById('toshi').value % 100 == 0
+   // &&
+  //  document.getElementById('toshi').value % 400 == 0
+    )
+    {alert('うるう年')}
+  else {alert('平年')}
+}
