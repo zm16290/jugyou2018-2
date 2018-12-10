a44632f824309338f55901cd251a101555d935f1
zm16290 <zm16290@josai.ac.jp>
2018-12-09 21:00:15 -0500

    1210

--- ex07-13.js
+++ ex07-13.js
@@ -1,12 +1,10 @@
 function dotira(){
-  if(document.getElementById('toshi').value % 4 == 0 
-    //||
-    //document.getElementById('tohsi').value % 4 == 0
-   // &&
-   // document.getElementById('toshi').value % 100 == 0
-   // &&
-  //  document.getElementById('toshi').value % 400 == 0
-    )
-    {alert('うるう年')}
-  else {alert('平年')}
-}
+if(document.getElementById('toshi').value % 4 != 0 )
+  {alert('平年')}
+   else
+   if(document.getElementById('toshi').value % 4 != 0 &&
+      document.getElementById('toshi').value % 100 == 0 &&
+      document.getElementById('toshi').value % 400 != 0)
+   {alert('平年')}
+  else{alert('うるう年')}
+  }
---
+++ ex08-2.js
@@ -0,0 +1,8 @@
+function seikai1(){
+  alert(640 * 480)
+}
+
+function seikai2(){
+  q = 640 *480
+  alert( q * q )
+}
