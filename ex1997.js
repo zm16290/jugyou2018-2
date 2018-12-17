6f9e090759e99e94508ccc7e88ff1e0e401118a9
zm16290 <zm16290@josai.ac.jp>
2018-12-16 20:57:07 -0500

    1217

--- ex1997.js
+++ ex1997.js
@@ -1,7 +1,7 @@
 function face1(){
-  document.getElementById('face').innerHTML='<img src="./img/ochanoma_mu.png">'
+  document.getElementById('face').innerHTML='<img src="./img/ochanoma_mu.png" id="kao1">'
 }
 
 function face2(){
-  document.getElementById('face').innerHTML='<img src="./img/ochanoma_smile.png">'
+  document.getElementById('face').innerHTML='<img src="./img/ochanoma_smile.png" id="kao2">'
 }
